7cffbd953adce7989350d41c980595868e576347
Jorge <hello@jhonboy.com>
2024-09-09 14:59:19 +0100

    👌 IMPROVE - Links and content

--- src/app/page.js
+++ src/app/page.js
@@ -5,26 +5,31 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
       <section>
-          <a>
+          <a href="https://www.jhonboy.com" target="_blank">
             <span>2024</span>
             <span>JHON BOY</span>
-            <span>ASTRO, CSS, JAVASCRIPT</span>
+            <span>ASTRO, JS</span>
           </a>
-          <a>
+          <a href="https://www.etifilm.com" target="_blank">
             <span>2024</span>
             <span>ETIFILM</span>
-            <span>NUXT, CSS, DOCKER, TYPESCRIPT</span>
+            <span>NUXT, DOCKER, TS</span>
           </a>
-          <a>
-            <span>2024</span>
+          <a href="https://jhonboyy.github.io/RISO_LAB/" target="_blank">
+            <span>2023</span>
             <span>RISO LAB</span>
-            <span>HTML, CSS, JAVASCRIPT</span>
+            <span>JS</span>
+          </a>
+          <a href="https://jhonboyy.github.io/DINO_GAME/" target="_blank" >
+            <span>2023</span>
+            <span>DINO GAME</span>
+            <span>JS</span>
           </a>
         </section>
         <header className={styles.header}>
           <h1>WEB DEVELOPMENT WITH A FOCUS ON DESIGN, EXPERIMENTATION, AND USER EXPERIENCE.</h1>
-          <a>CONTACT</a>
-          <a className={styles.githubLink}>GITHUB <span className={styles.arrow}>&#8599;</span></a>
+          <a href="mailto:hello@jhonboy.com">CONTACT</a>
+          <a href="https://github.com/jhonboyy" target="_blank" className={styles.githubLink}>GITHUB <span className={styles.arrow}>&#8599;</span></a>
         </header>
       </main>
     </div>
